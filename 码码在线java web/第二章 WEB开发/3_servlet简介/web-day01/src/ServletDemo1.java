c486e42490e9b55686e450fd62d3012ba8da570e
wangchi <291223448@qq.com>
2020-03-30 17:41:53 +0800

    java web

---
+++ 码码在线java web/第二章 WEB开发/3_servlet简介/web-day01/src/ServletDemo1.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class ServletDemo1 extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("hello servlet 我是demo1 post1");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("hello servlet 我是demo1");
+    }
+}
